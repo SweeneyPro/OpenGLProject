9f71824497f285895d28793d37e3dc4fff408538
SweeneyPro <sweeney.pro@hotmail.co.uk>
2017-06-15 14:51:15 +0100

    Create mesh.cpp

--- mesh.cpp
+++ mesh.cpp
@@ -28,60 +28,7 @@ Mesh::Mesh(Vertex* vertices, unsigned int numVertices, unsigned int* indices, un
 		model.indices.push_back(indices[i]);
 	}
 	InitMesh(model);
-	/*
-	m_drawCount = numIndicies; // Draw all verticies
-	glGenVertexArrays(1, &m_vertexArrayObject);	// Generate internal vertex array, 1 array and its our vertexarrayobject
-	glBindVertexArray(m_vertexArrayObject);	// Bind vertex array object so any opengl stuff will affect this bound vertex array object
-
-	std::vector<glm::vec3> positions;
-	std::vector<glm::vec2> texCoords;
-
-	positions.reserve(numVertices);
-	texCoords.reserve(numVertices);
-
-	for (unsigned int i = 0; i < numVertices; i++)
-	{
-		positions.push_back(*vertices[i].getPos());
-		texCoords.push_back(*vertices[i].getTexCoord());
-
-	}
-
-	glGenBuffers(NUM_BUFFERS, m_vertexArrayBuffers);
-	glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[POSITION_VB]);
-	glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(positions[0]), &positions[0], GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	
-	glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[TEXCOORD_VB]);
-	glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(texCoords[0]), &texCoords[0], GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_vertexArrayBuffers[INDEX_VB]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, numIndicies * sizeof(indices[0]), &indices[0], GL_STATIC_DRAW);
-
-	glBindVertexArray(0);
-
-	*/
-
-	/*
-	glGenBuffers(NUM_BUFFERS, m_vertexArrayBuffers);	// Creates a buffer  (Buffer is block of data)
-	glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[POSITION_VB]);	// GL_ARRAY_BUFFER, tells opengl we are giving it an array for the buffer
-	glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(positions[0]), &positions[0], GL_STATIC_DRAW);	// takes data and sends it to the gfx card
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT,GL_FALSE, 0, 0);
-	//glBindVertexArray(0);
-
-	//glGenBuffers(NUM_BUFFERS, m_vertexArrayBuffers);	// Creates a buffer  (Buffer is block of data)
-	glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[TEXCOORD_VB]);	// GL_ARRAY_BUFFER, tells opengl we are giving it an array for the buffer
-	glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(texCoords[0]), &texCoords[0], GL_STATIC_DRAW);	// takes data and sends it to the gfx card
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
-	//glBindVertexArray(1);	// bind vertex array object now we have finished editing it
-	glBindVertexArray(0);
-	*/
 }
 
 
@@ -125,7 +72,6 @@ void Mesh::InitMesh(const IndexedModel& model)
 void Mesh::Draw()
 {
 	glBindVertexArray(m_vertexArrayObject);
-	//glDrawArrays(GL_TRIANGLES, 0, m_drawCount);	// Draws - What type of shapes, any offset from the start, how far into the array
 	glDrawElements(GL_TRIANGLES, m_drawCount, GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 
